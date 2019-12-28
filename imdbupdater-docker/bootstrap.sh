d211919345e4ff86e324a641169b2b847984066b
Marc Herschel <marc@herschel.io>
2019-12-28 17:33:31 +0100

    docker container

---
+++ imdbupdater-docker/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION_URL=https://raw.githubusercontent.com/mynttt/PlexImdbUpdateTool/master/VERSION
+
+function download() {
+    VERSION=$1
+    echo "**** Downloading version ${VERSION} ****"
+    __DURL="https://github.com/mynttt/PlexImdbUpdateTool/releases/download/${VERSION}/PlexImdbUpdateTool-${VERSION}.jar"
+    wget -O tool.jar $__DURL #2>&1 | grep "^wget:"
+}
+
+if [ ! -f VERSION ]; then
+    wget -O VERSION $VERSION_URL 2>&1 | grep "^wget:"
+    download $(cat VERSION)
+else
+    wget -O VERSION2 $VERSION_URL 2>&1 | grep "^wget:"
+    if [[ ! $(cat VERSION) = $(cat VERSION2) ]]; then
+        download $(cat VERSION2)
+        mv VERSION2 VERSION
+    else
+        rm VERSION2
+    fi
+fi
---
+++ imdbupdater-docker/entry.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /usr/app
+./bootstrap.sh
+cd /config
+PLEX_DATA_DIR="/plexdata"
+export PLEX_DATA_DIR
+echo "**** Invoking tool! Logs in /config ****"
+java -XX+UseG1GC -Xms32m -jar /usr/app/tool.jar imdb-docker $RUN_EVERY_N_HOURS $CLEAR_CACHE_EVERY_N_DAYS
