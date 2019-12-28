bf0f69f46cfffd95bff7a10ab3db2667484c802d
Marc Herschel <m7kisonline@gmail.com>
2019-12-28 18:10:26 +0100

    Update entry.sh

--- imdbupdater-docker/entry.sh
+++ imdbupdater-docker/entry.sh
@@ -5,4 +5,4 @@ cd /config
 PLEX_DATA_DIR="/plexdata"
 export PLEX_DATA_DIR
 echo "**** Invoking tool! Logs in /config ****"
-java -XX+UseG1GC -Xms32m -jar /usr/app/tool.jar imdb-docker $RUN_EVERY_N_HOURS $CLEAR_CACHE_EVERY_N_DAYS
+java -Xms32m -jar /usr/app/tool.jar imdb-docker $RUN_EVERY_N_HOURS $CLEAR_CACHE_EVERY_N_DAYS
