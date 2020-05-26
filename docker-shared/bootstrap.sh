f5a2e124c2e16f9013b71e0c8b5ab86dabfcd62b
Marc Herschel <m7kisonline@gmail.com>
2020-05-26 15:26:57 +0200

    Docker: Removed auto-patch on startup. Warning will be emitted if older version is in use.

--- docker-shared/bootstrap.sh
+++ docker-shared/bootstrap.sh
@@ -14,10 +14,12 @@ if [ ! -f VERSION ]; then
     download $(cat VERSION)
 else
     wget -O VERSION2 $VERSION_URL 2>&1 | grep "^wget:"
-    if [[ ! $(cat VERSION) = $(cat VERSION2) ]]; then
-        download $(cat VERSION2)
-        mv VERSION2 VERSION
+    V_CUR=$(cat VERSION)
+    V_REP=$(cat VERSION2)
+    if [[ ! "$V_CUR" = "$V_REP" ]]; then
+        echo "*** OUTDATED VERSION USED !!! DOCKER IS ON ${V_CUR} WHILE CURRENT VERSION IS ${V_REP}. PLEASE UPDATE YOUR CONTAINER! ***"
     else
-        rm VERSION2
+        echo "*** VERSION ${V_CUR} IS UP TO DATE! ***"
     fi
+    rm VERSION2
 fi
