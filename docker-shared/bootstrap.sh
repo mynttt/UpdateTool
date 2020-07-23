402e8678c0042a44ec04298a727b07cd9261d263
Marc Herschel <m7kisonline@gmail.com>
2020-07-23 10:47:44 +0200

    Docker: Moved downloading into build process so nothing weird happens when no internet access is given and the version check fails

--- docker-shared/bootstrap.sh
+++ docker-shared/bootstrap.sh
@@ -2,22 +2,15 @@
 
 VERSION_URL=https://raw.githubusercontent.com/mynttt/UpdateTool/master/VERSION
 
-function download() {
-    VERSION=$1
-    echo "**** Downloading version ${VERSION} ****"
-    __DURL="https://github.com/mynttt/UpdateTool/releases/download/${VERSION}/UpdateTool-${VERSION}.jar"
-    wget -O tool.jar $__DURL #2>&1 | grep "^wget:"
-}
-
 if [ ! -f VERSION ]; then
-    wget -O VERSION $VERSION_URL 2>&1 | grep "^wget:"
-    download $(cat VERSION)
+    echo "@@ CRITICAL !!! - NO VERSION FILE FOUND WITHIN IMAGE !!! @@"
+    exit 1
 else
     wget -O VERSION2 $VERSION_URL 2>&1 | grep "^wget:"
     V_CUR=$(cat VERSION)
     V_REP=$(cat VERSION2)
     if [[ ! "$V_CUR" = "$V_REP" ]]; then
-        echo "*** OUTDATED VERSION USED !!! DOCKER IS ON ${V_CUR} WHILE CURRENT VERSION IS ${V_REP}. PLEASE UPDATE YOUR CONTAINER! ***"
+        echo "*** OUTDATED VERSION USED (OR NO INTERNET ACCESS) !!! DOCKER IS ON ${V_CUR} WHILE CURRENT VERSION IS ${V_REP}. PLEASE UPDATE YOUR CONTAINER! ***"
     else
         echo "*** VERSION ${V_CUR} IS UP TO DATE! ***"
     fi
---
+++ docker-shared/download.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+VERSION_URL=https://raw.githubusercontent.com/mynttt/UpdateTool/master/VERSION
+wget -O VERSION "$VERSION_URL" 2>&1 | grep "^wget:"
+VERSION=$(cat "VERSION")
+echo "**** Downloading version ${VERSION} ****"
+__DURL="https://github.com/mynttt/UpdateTool/releases/download/${VERSION}/UpdateTool-${VERSION}.jar"
+wget -O tool.jar "$__DURL" || exit 1
