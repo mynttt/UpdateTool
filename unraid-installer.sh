a1bfd75cd7441620f453d234a13bbcf9ce89cc5e
mynttt <marc@herschel.io>
2023-02-15 02:36:30 +0100

    work on unraid standalone installer

---
+++ unraid-installer.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+set -e
+
+MASTER_URL="https://raw.githubusercontent.com/mynttt/UpdateTool/master/unraid-installer.zip"
+
+echo "***************************"
+echo "Welcome to the UpdateTool UnRaid/Linux standalone installer!"
+echo "You are executing this script in ${PWD}"
+echo "***************************"
+echo "Are you sure to install this tool here in ${PWD}? [yes/no]"
+read x
+if [ "$x" != "yes" ]
+then
+    echo "Installer aborted due to user cancelation."
+    exit 1
+fi
+echo "Downloading installer and dependencies..."
+wget --no-cache -O "updatetool-installer.zip" "$MASTER_URL" 2>&1 | grep "^wget:"
+unzip "updatetool-installer.zip"
+rm "updatetool-installer.zip"
+echo "Starting installer... Make sure to follow instructions!"
+chmod +x install.sh
+chmod +x dependencies/*
+./install.sh
+echo "Installer has ended."
