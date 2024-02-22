e92ab8f9cf5e9e46ff407b1e6a8636846a6bfa8c
mynttt <marc@herschel.io>
2024-02-22 18:08:30 +0100

    removed unused class

--- src/main/java/updatetool/imdb/ImdbOnDemandImplementation.java
+++
@@ -1,9 +0,0 @@
-package updatetool.imdb;
-
-public class ImdbOnDemandImplementation extends ImdbDockerImplementation {
-
-    public ImdbOnDemandImplementation(String id, String desc, String usage, String help) {
-        super(id, desc, usage, help);
-    }
-
-}
